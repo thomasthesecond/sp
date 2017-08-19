b346eb2d98e45535fb85511ffac8cdaf5f2b87d3
Tom Cunningham <tom.cunningham@lonelyplanet.com>
2017-08-19 01:06:31 -0500

    Add cover video to home

--- src/assets/home.js
+++ src/assets/home.js
@@ -2,14 +2,17 @@
 import "../components/05-pages/home";
 
 // Import page-specific components
+import CoverVideo from "../components/03-components/cover-video";
 import "../components/03-components/image-block";
 import "../components/03-components/introduction";
 import "../components/03-components/masthead";
 import LocationFinder from "../components/03-components/location-finder";
 import "../components/03-components/location-finder-button";
 
+const coverVideo = new CoverVideo();
 const locationFinder = new LocationFinder();
 
+coverVideo.render();
 locationFinder.render();
 
 console.info("Home loaded");
