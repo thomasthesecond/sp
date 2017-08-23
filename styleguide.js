d7197e5f69fb88c79456e4d9f8c93fe52acf07c4
Tom Cunningham <tom.cunningham@lonelyplanet.com>
2017-08-23 03:06:22 -0500

    Updates

--- styleguide.js
+++ styleguide.js
@@ -3691,7 +3691,7 @@ console.info("Styleguide loaded");
 if ("objectFit" in document.documentElement.style === false) {
   document.addEventListener("DOMContentLoaded", function () {
     Array.prototype.forEach.call(document.querySelectorAll(".CoverPhoto img"), function (image) {
-      (image.runtimeStyle || image.style).background = "url(\"" + image.src + "\") no-repeat 50%/cover)";
+      (image.runtimeStyle || image.style).background = "url(\"" + image.src + "\") no-repeat 50% / cover";
 
       image.src = "data:image/svg+xml,%3Csvg xmlns=\"http://www.w3.org/2000/svg\" width=\"" + image.width + "\" height=\"" + image.height + "\" %3E%3C/svg%3E";
     });
