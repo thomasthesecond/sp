ab6584a70310c62896102d261d9720426fbbc6d7
Tom Cunningham <tom.cunningham@lonelyplanet.com>
2017-08-25 00:28:39 -0500

    Create objectFitCover util

--- src/assets/js/utils.js
+++ src/assets/js/utils.js
@@ -1,11 +1,32 @@
 "use strict";
 
+/**
+ * forEach for NodeList
+ */
 const forEach = (array, callback, scope) => {
   for (let i = 0; i < array.length; i++) {
     callback.call(scope, i, array[i]);
   }
 };
 
+/**
+ * Polyfill object-fit: cover
+ */
+const objectFitCover = () => {
+  if ("objectFit" in document.documentElement.style === false) {
+    document.addEventListener("DOMContentLoaded", () => {
+      const nodes = document.querySelectorAll(".object-fit-cover");
+
+      forEach(nodes, (index, node) => {
+        const image = node.querySelector("img");
+        (node.runtimeStyle || node.style).background = `url("${image.src}") no-repeat 50% / cover`;
+        (image.runtimeStyle || image.style).display = "none";
+      });
+    });
+  }
+};
+
 export {
   forEach,
+  objectFitCover,
 };
