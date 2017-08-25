6fb1deb4b622f308bcdd9f855ec0a123401076ba
Tom Cunningham <tom.cunningham@lonelyplanet.com>
2017-08-25 00:15:51 -0500

    Create JS utils

---
+++ src/assets/js/utils.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const forEach = (array, callback, scope) => {
+  for (let i = 0; i < array.length; i++) {
+    callback.call(scope, i, array[i]);
+  }
+};
+
+export {
+  forEach,
+};
