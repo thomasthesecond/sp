637e3c86572ea732e21ac9fe2d5b42f88ff1e964
Tom Cunningham <tom.cunningham@lonelyplanet.com>
2017-08-27 09:44:30 -0500

    Clean up webpack config, add production config

--- webpack.config.js
+++ webpack.config.js
@@ -1,52 +1,73 @@
+const production = process.env.NODE_ENV === "production";
 const path = require("path");
+const webpack = require("webpack");
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
+const UglifyJSPlugin = require("uglifyjs-webpack-plugin");
 const autoprefixer = require("autoprefixer");
 
 const loaders = {
   css: {
     loader: "css-loader",
     options: {
-      minimize: false
-    }
+      minimize: production,
+    },
   },
   postcss: {
     loader: "postcss-loader",
     options: {
-      plugins: (loader) => [
-        autoprefixer()
-      ]
-    }
+      plugins: () => [
+        autoprefixer(),
+      ],
+    },
   },
   sass: {
     loader: "sass-loader",
     options: {
       outputStyle: "expanded",
-      // indentedSyntax: false,
       includePaths: [
-        path.resolve(__dirname, "./node_modules")
+        path.resolve(__dirname, "./node_modules"),
       ],
-    }
-  }
+    },
+  },
+};
+
+const filename = production ? "[name].min" : "[name]";
+
+const plugins = [
+  new ExtractTextPlugin(`${filename}.css`),
+  new ExtractTextPlugin(`${filename}.css`),
+  new CopyWebpackPlugin([
+    {
+      from: "src/assets/images",
+      to: "images",
+    },
+  ]),
+  new webpack.ProvidePlugin({
+    $: "jquery",
+    jQuery: "jquery",
+    jquery: "jquery",
+  }),
+];
+
+if (production) {
+  plugins.push(new UglifyJSPlugin());
 }
 
 module.exports = {
-  // entry: "./src/index.js",
   entry: {
     main: "./src/assets/main.js",
-    // home: ["./src/assets/main.js", "./src/assets/home.js"],
-    // about: ["./src/assets/main.js", "./src/assets/about.js"],
     home: "./src/assets/home.js",
     about: "./src/assets/about.js",
     locations: "./src/assets/locations.js",
     investors: "./src/assets/investors.js",
     partnerships: "./src/assets/partnerships.js",
     contact: "./src/assets/contact.js",
-    styleguide: "./src/assets/styleguide.js"
+    styleguide: "./src/assets/styleguide.js",
   },
   output: {
     path: path.resolve(__dirname, "tmp/assets"),
-    filename: "[name].js"
+    filename: `${filename}.js`,
   },
   module: {
     rules: [
@@ -54,77 +75,33 @@ module.exports = {
         test: /(\.js?)$/,
         loader: "babel-loader",
         query: {
-          "plugins": ["transform-decorators-legacy"],
-          "presets": ["es2015"]
-        }
+          plugins: ["transform-decorators-legacy"],
+          presets: ["es2015"],
+        },
       },
       {
         test: /\.css$/,
         loader: ExtractTextPlugin.extract({
           fallback: "style-loader",
-          use: loaders.css
+          use: loaders.css,
         }),
       },
       {
         test: /\.scss$/,
         use: ExtractTextPlugin.extract({
           fallback: "style-loader",
-          // use: ["css-loader", "sass-loader"]
-          use: [loaders.css, loaders.postcss, loaders.sass]
-        })
+          use: [loaders.css, loaders.postcss, loaders.sass],
+        }),
       },
       {
         test: /\.hbs$/,
-        loader: "handlebars-loader"
+        loader: "handlebars-loader",
       },
       {
         test: /\.jpg$|\.png$|\.svg$/,
-        loader: "file-loader?name=[name].[ext]"
-      }
-    ]
+        loader: "file-loader?name=[name].[ext]",
+      },
+    ],
   },
-  plugins: [
-    new ExtractTextPlugin("[name].css"),
-    new CopyWebpackPlugin([
-      { from: "src/assets/images", to: "images" }
-    ])
-  ]
+  plugins,
 };
-
-// const config = {
-//   entry: {
-//     app: ["./src/index"]
-//   },
-//
-//   module: {
-//     rules: [
-//       // {
-//       //   test: /\.js$/,
-//       //   exclude: /node_modules/,
-//       //   use: ["babel-loader"]
-//       // },
-//       {
-//         test: /\.scss$/,
-//         use: ExtractTextPlugin.extract({
-//           fallback: "style-loader",
-//           use: [loaders.css, loaders.postcss, loaders.sass]
-//         })
-//       }
-//     ]
-//   },
-//
-//   output: {
-//     filename: "[name].js",
-//     path: path.join(__dirname, "./public"),
-//     publicPath: "/public"
-//   },
-//
-//   plugins: [new ExtractTextPlugin("[name].css")],
-//
-//   resolve: {
-//     extensions: [".js", ".scss"],
-//     modules: [path.join(__dirname, "./src"), "node_modules"]
-//   }
-// };
-//
-// module.exports = config;
